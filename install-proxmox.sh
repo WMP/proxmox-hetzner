c466a8be1fda4f29377c6a36c991c39ee0650bec
Marcin Janowski <marcin.janowski@assecobs.pl>
2024-11-01 21:05:50 +0100

    fix in help

--- install-proxmox.sh
+++ install-proxmox.sh
@@ -34,7 +34,7 @@ show_help() {
     for plugin in $(echo "$plugin_list" | tr ',' '\n'); do
         if [[ "$(describe_plugin "$plugin")" == *"[Optional]"* ]]; then
             echo "  $plugin:"
-            describe_plugin "$plugin" true | sed 's/^/    /'
+            describe_plugin "$plugin" true | sed 's/^/    /' | tail -n +2
         fi
     done
     
@@ -43,7 +43,7 @@ show_help() {
     for plugin in $(echo "$plugin_list" | tr ',' '\n'); do
         if [[ "$(describe_plugin "$plugin")" == *"[Default]"* ]]; then
             echo "  $plugin:"
-            describe_plugin "$plugin" true | sed 's/^/    /'
+            describe_plugin "$plugin" true | sed 's/^/    /' | tail -n +2
         fi
     done
 }
@@ -53,63 +53,53 @@ describe_plugin() {
         "run_tteck_post-pve-install")
             echo "[Default]"
             echo "Run additional post-installation tasks from https://tteck.github.io/Proxmox/"
-            echo
             ;;
         "set_network")
             echo "[Default]"
             echo "Configure network settings based on Hetzner rescue network"
-            echo
             ;;
         "update_locale_gen")
             echo "[Default]"
             echo "Update locale settings with your ssh_client LC_NAME: ${LC_NAME}"
-            echo
             ;;
         "register_acme_account")
             echo "[Optional]"
             echo "Registers an ACME account for Let's Encrypt SSL certificate."
             echo "Required options:"
-            echo "  --acme-email EMAIL     Set email for ACME account"
-            echo
+            echo "    --acme-email EMAIL     Set email for ACME account"
             ;;
         "disable_rpcbind")
             echo "[Default]"
             echo "Disable rpcbind service"
-            echo
             ;;
         "install_iptables_rule")
             echo "[Default]"
             echo "Install custom iptables rule"
-            echo
             ;;
         "add_ssh_key_to_authorized_keys")
             echo "[Optional]"
             echo "Adds SSH public key to authorized_keys."
             echo "Required options:"
-            echo "  --ssh-key SSH_KEY     Add SSH public key to authorized_keys (must be a path to .pub file)"
-            echo
+            echo "    --ssh-key SSH_KEY     Add SSH public key to authorized_keys (must be a path to .pub file)"
             ;;
         "change_ssh_port")
             echo "[Optional]"
             echo "Changes the default SSH port for Proxmox server."
             echo "Required options:"
-            echo "  --port PORT           Set the new SSH port"
-            echo
+            echo "    --port PORT           Set the new SSH port"
             ;;
         "add_tun_lxc_device")
             echo "[Default]"
             echo "Add default configuration to LXC containers to create a tun interface"
-            echo
             ;;
         "zabbix_agent")
             echo "[Optional]"
             echo "Installs and configures Zabbix Agent."
             echo "Required options:"
-            echo "  --zabbix-server ADDRESS     Set Zabbix Server address"
+            echo "    --zabbix-server ADDRESS          Set Zabbix Server address"
             echo "Optional parameters:"
-            echo "  --zabbix-agent-version VERSION  Specify Zabbix Agent version"
-            echo "  --zabbix-hostname HOSTNAME       Set hostname for Zabbix Agent"
-            echo
+            echo "    --zabbix-agent-version VERSION   Specify Zabbix Agent version"
+            echo "    --zabbix-hostname HOSTNAME       Set hostname for Zabbix Agent"
             ;;
         *)
             echo "No description available"
